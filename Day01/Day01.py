1fc91f3155c5eca671f3066922672b4e1b99eece
Paul <pcrodgers1@gmail.com>
2020-12-01 08:36:11 -0700

    Day 1 solution

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -4,3 +4,14 @@ sys.path.append('./')
 from adventInput import GetInput
 
 _input = GetInput(1)
+
+for x in _input:
+  for y in _input:
+    for z in _input:
+      x = int(x)
+      z = int(z)
+      y = int(y)
+      if x != y and x != z and y != z:
+        if x + y + z == 2020:
+          print(x*y*z)
+          break
