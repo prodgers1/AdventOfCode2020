0588c1c47afb144ef46c3ba5ee3658d789a3f963
Paul <pcrodgers1@gmail.com>
2020-12-01 22:56:05 -0700

    Day 2

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -4,3 +4,39 @@ sys.path.append('./')
 from adventInput import GetInput
 
 _input = GetInput(2)
+valid = list()
+
+for rule in _input:
+  ruleSplit = rule.split(' ')
+  nums = ruleSplit[0].split('-')
+  lowBound = int(nums[0])
+  highBound = int(nums[1])
+  neccessaryChar = ruleSplit[1].split(":")[0]
+
+  password = ruleSplit[2]
+
+  charCount = password.count(neccessaryChar)
+
+  if charCount < lowBound or charCount > highBound:
+    continue
+
+  valid.append(rule)
+
+print(len(valid))
+valid.clear()
+
+for rule in _input:
+  ruleSplit = rule.split(' ')
+  nums = ruleSplit[0].split('-')
+  firstPosition = int(nums[0]) - 1
+  lastPosition = int(nums[1]) - 1
+
+  neccessaryChar = ruleSplit[1].split(":")[0]
+
+  password = ruleSplit[2]
+
+  if ((password[firstPosition] == neccessaryChar or password[lastPosition] == neccessaryChar) and
+    not(password[firstPosition] == neccessaryChar and password[lastPosition] == neccessaryChar)):
+    valid.append(rule)
+
+print(len(valid))
