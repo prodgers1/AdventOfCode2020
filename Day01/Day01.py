162eb06a4826c36885e72d2ed7122200f252fffc
Paul <pcrodgers1@gmail.com>
2020-12-01 21:55:44 -0700

    Day 1 more pythony

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -5,13 +5,16 @@ from adventInput import GetInput
 
 _input = GetInput(1)
 
-for x in _input:
-  for y in _input:
-    for z in _input:
-      x = int(x)
-      z = int(z)
-      y = int(y)
-      if x != y and x != z and y != z:
-        if x + y + z == 2020:
-          print(x*y*z)
-          break
+[print(int(x)*int(y)) for x in _input for y in _input if int(x)+int(y) == 2020 ]
+[print(int(x)*int(y)*int(z)) for x in _input for y in _input for z in _input if int(x)+int(y)+int(z) == 2020 ]
+
+# for x in _input:
+#   for y in _input:
+#     for z in _input:
+#       x = int(x)
+#       z = int(z)
+#       y = int(y)
+#       if x != y and x != z and y != z:
+#         if x + y + z == 2020:
+#           print(x*y*z)
+#           break
