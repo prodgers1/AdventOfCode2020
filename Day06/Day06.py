029e667cb2e77fc9a0903b40e7bcda12f81575f3
Paul <pcrodgers1@gmail.com>
2020-12-06 00:40:20 -0700

    Day 6

--- Day06/Day06.py
+++ Day06/Day06.py
@@ -5,3 +5,32 @@ sys.path.append('./')
 from adventInput import GetInput
 
 _input = GetInput(6)
+
+indicies = [i for i,x in enumerate(_input) if x == ""]
+indicies.append(len(_input))
+i = 0
+
+part1YesAnswers = 0
+part2YesAnswers = 0
+
+for index in indicies:
+  group = _input[i:index]
+  part1 = list()
+  part2 = list()
+  for member in group:
+    memberQuestionAnswered = set()
+    for question in member:
+      memberQuestionAnswered.add(question)
+      if question not in part1:
+        part1.append(question)
+        part1YesAnswers += 1
+    part2.append(memberQuestionAnswered)
+
+  result = part2[0]
+  for l in part2[1:]:
+    result = result.intersection(l)
+  part2YesAnswers += len(result)
+  i = index + 1
+
+print(part1YesAnswers)
+print(part2YesAnswers)
