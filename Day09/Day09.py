8c6269976b00a58077e4a6053fe7807aa286c245
Paul <pcrodgers1@gmail.com>
2020-12-08 23:02:21 -0700

    day 9 python sucks at off by 1s

--- Day09/Day09.py
+++ Day09/Day09.py
@@ -3,7 +3,56 @@ import math
 import time
 sys.path.append('./')
 from adventInput import GetInput
-from collections import defaultdict
 import copy
+import itertools
+import collections
 
 _input = GetInput(9)
+
+preambleSize = 25
+startIndex = 0
+
+endIndex = preambleSize
+
+while True:
+  currentPreamble =  _input[startIndex:endIndex] 
+  if endIndex + 1 >= len(_input):
+    break
+
+  target = int(_input[endIndex])
+  valid = False
+  for c in currentPreamble:
+    toFind = target - int(c)
+    if str(toFind) in currentPreamble:
+      valid = True
+      break
+      
+  if not valid:
+    notValid.append(target)
+    print(target)
+    break
+  
+  startIndex += 1
+  endIndex += 1
+
+target = 10884537
+
+i = 0
+for current in _input:
+  
+  currentSum = 0
+  tried = list()
+  offset = 0
+  while currentSum < target:
+    toAdd = _input[i+offset]
+    currentSum += int(toAdd)
+    tried.append(toAdd)
+    offset += 1
+  
+  i+=1
+
+  if currentSum == target:
+    sortedList = sorted(tried)
+    print(int(sortedList[0]) + int(sortedList[-1]))
+    break
+
