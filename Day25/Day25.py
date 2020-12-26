e54830d8650ccafbe6131461f838d57bfc1bbb97
Paul <pcrodgers1@gmail.com>
2020-12-25 21:28:28 -0700

    refactor 25

--- Day25/Day25.py
+++ Day25/Day25.py
@@ -13,17 +13,19 @@ def transform(subject, loopSize):
   #raise subject to loopsize exponent, then mod that answer by the third parameter
   return pow(subject, loopSize, 20201227)
 
+def findLoopSize(publicKey):
+  potentialLoopSize = 1
+  while transform(7, potentialLoopSize) != publicKey:
+    potentialLoopSize += 1
+  
+  return potentialLoopSize
+
+
 cardPublicKey = int(_input[0])
 doorPublicKey = int(_input[1])
 
-cardLoopSize = 1
-doorLoopSize = 1
-
-while transform(7, cardLoopSize) != cardPublicKey:
-  cardLoopSize += 1
-
-while transform(7, doorLoopSize) != doorPublicKey:
-  doorLoopSize += 1
+cardLoopSize = findLoopSize(cardPublicKey)
+doorLoopSize = findLoopSize(doorPublicKey)
 
 encryptionKey = transform(cardPublicKey, doorLoopSize)
 print(encryptionKey)
